67fc7d4d8492f69be837d3b2c79ce96d0b52cef5
Aryav <tiwarikaustav@gmail.com>
2020-11-16 08:18:00 +0530

    [Aryav] Created DB AddressBookModel.cs

---
+++ AddressBookModel.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookModel.cs" company="Bridgelabz">
+//  Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aryav Tiwari"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace AddressBookProblemLINQ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// Class to map the relational data base model to a entity
+    /// Contains fields mimicing the exact replica of that of the table level
+    /// </summary>
+    public class AddressBookModel
+    {
+        public int contactID { get; set; }
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public long zip { get; set; }
+        public long phoneNumber { get; set; }
+        public string emailId { get; set; }
+        public string contactType { get; set; }
+        public string addressBookName { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aryav Tiwari"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace AddressBookProblemLINQ
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Welcome to the Address Book Data LINQ Program");
+            Console.WriteLine("========================================================");
         }
     }
 }
